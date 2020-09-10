33a50eccc44dc2def46adb121bac7e4a13dda6c1
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-09-10 14:50:07 +0100

    added function to clear terminal

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "./rpn.cpp"
 
+#include <iostream>
+#include <string>
+
+// prints character to reset terminal. This has side-effects, work on it later
+void clear_screen()
+{
+    std::cout << "\033c";
+}
+
 int main()
 {
     rpnstack test;
+
+    clear_screen();
+
     test.push(1);
     test.push(2);
     test.push(3);
