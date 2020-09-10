2a48d701ee798bea6c3c085a04676ad533f6afa8
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-09-10 18:01:04 +0100

    added very basic UI

--- main.cpp
+++ main.cpp
@@ -3,27 +3,50 @@
 #include <iostream>
 #include <string>
 
+void clear_screen();
+bool is_number(std::string);
+
+int main()
+{
+    rpnstack test;
+
+    std::cout << "Exit with ^C\n" << std::endl;
+
+    while (true) {
+        std::string inp;
+        std::cin >> inp;
+        if (is_number(inp))
+            test.push(std::stod(inp));
+        else
+            if (inp == "+" || inp == "add") {
+                test.add();
+            }
+            if (inp == "-" || inp == "sub") {
+                test.add();
+            }
+            if (inp == "*" || inp == "mult") {
+                test.add();
+            }
+            if (inp == "/" || inp == "div") {
+                test.add();
+            }
+        clear_screen();
+        test.print_stack();
+    }
+
+    return 0;
+}
+
 // prints character to reset terminal. This has side-effects, work on it later
 void clear_screen()
 {
     std::cout << "\033c";
 }
 
-int main()
-{
-    rpnstack test;
-
-    clear_screen();
-
-    test.push(1);
-    test.push(2);
-    test.push(3);
-    test.push(4);
-
-    test.print_stack();
-
-    test.add();
-
-    test.print_stack();
-    return 0;
+// checks if the input is a number, floating point true
+bool is_number(std::string inp) {
+    for (char i : inp)
+        if (!isdigit(i) && i != '.')
+            return false;
+    return true;
 }
