8c4a1e4a38e37ae0e309039e1f1cc2263fa24e17
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-09-10 18:06:36 +0100

    formatting main.cpp

--- main.cpp
+++ main.cpp
@@ -17,19 +17,18 @@ int main()
         std::cin >> inp;
         if (is_number(inp))
             test.push(std::stod(inp));
-        else
-            if (inp == "+" || inp == "add") {
-                test.add();
-            }
-            if (inp == "-" || inp == "sub") {
-                test.add();
-            }
-            if (inp == "*" || inp == "mult") {
-                test.add();
-            }
-            if (inp == "/" || inp == "div") {
-                test.add();
-            }
+        else if (inp == "+" || inp == "add") {
+            test.add();
+        }
+        if (inp == "-" || inp == "sub") {
+            test.add();
+        }
+        if (inp == "*" || inp == "mult") {
+            test.add();
+        }
+        if (inp == "/" || inp == "div") {
+            test.add();
+        }
         clear_screen();
         test.print_stack();
     }
@@ -44,7 +43,8 @@ void clear_screen()
 }
 
 // checks if the input is a number, floating point true
-bool is_number(std::string inp) {
+bool is_number(std::string inp)
+{
     for (char i : inp)
         if (!isdigit(i) && i != '.')
             return false;
