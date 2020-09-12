4733a65074209b2fde35fdfb06ece32469497742
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-09-12 13:17:30 +0100

    main.cpp: better commenting

--- main.cpp
+++ main.cpp
@@ -17,8 +17,10 @@ int main()
         std::string inp;
         std::cin >> inp;
         double num = 0;
+        // This bit feels kinda bad, anyone know how to make it less bad?
         if (parse_number(inp, num))
             s.push(num);
+        // All the various operations are here :-)
         else if (inp == "+" || inp == "add")
             s.add();
         else if (inp == "-" || inp == "sub")
@@ -31,14 +33,15 @@ int main()
             s.mod();
         else if (inp == "^" || inp == "pow")
             s.pow();
-        else if (inp == "log")
+        else if (inp == "log") // *arbitrary* log
             s.log();
         else if (inp == "log10")
             s.log10();
         else if (inp == "log2")
             s.log2();
-        else if (inp == "ln")
+        else if (inp == "ln") // natural log
             s.ln();
+        // operations more to do with the stack than the numbers in it
         else if (inp == "del")
             s.del();
         else if (inp == "swap")
@@ -48,20 +51,20 @@ int main()
         else if (inp == "down")
             s.cycle_down();
 
-        clear_screen();
+        clear_screen(); // clears the screen before printing the new stack
         s.print_stack();
     }
 
     return 0;
 }
 
-// prints character to reset terminal. This has side-effects, work on it later
+// prints character to reset terminal. This may have side-effects, work on it later
 void clear_screen()
 {
     std::cout << "\033c";
 }
 
-// checks if the input is a number, floating point true
+// checks if the input is a number, also converts it to a double if it can be parsed
 bool parse_number(const std::string &inp, double &num)
 {
     char dummy;
